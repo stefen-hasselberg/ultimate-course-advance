27150c04802efe32cbab7f5df03ed10b5efdb01a
Stefen Hasselberg <Stefen.Hasselberg@emergeaotearoa.org.nz>
2021-09-06 09:44:56 +1200

    updated project to use webpack for bundling

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      {
+        test: /.ts?S/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [new HtmlWebpackPlugin({ title: 'Typescript Webpack' })],
+};
